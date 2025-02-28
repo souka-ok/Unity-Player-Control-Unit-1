b3a72a93012d0876ebae1dc1995404fdb45edabe
Lopez_Anthonee <lopea468@hsd.k12.or.us>
2025-02-28 11:06:40 -0800

    finished 1.3

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    float speed = 5.0f;
+    public float speed = 5.0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
